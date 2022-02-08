5f4b02fa7ec4f5de141aabc24cdd687d9cd0f667
Brian Kiplangat <brian.kiplangat@student.moringaschool.com>
2022-02-08 13:58:54 +0300

    intial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { CountPipe } from './count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuotesComponent,
+    CountPipe,
+    QuoteFormComponent,
+    QuoteDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit,Input,EventEmitter,Output } from '@angular/core';
+import { Quotes } from 'src/app/quotes';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+
+  faCoffee = faTrash;
+  faThumbsDown=faThumbsDown;
+  faThumbsUp = faThumbsUp;
+
+  
+  // dislike: number =0;
+  @Input() quote!:Quotes;
+  @Output() remove = new EventEmitter<boolean>();
+  noOflike: number =0;
+  delete(complete:boolean){
+    this.remove.emit(complete);
+  }
+  constructor() { }
+ 
+  Like(){
+    this.noOflike++; 
+  }
+  Dislike(){
+    this.noOflike--;
+  }
+  ngOnInit(): void {
+  }
+
+}
